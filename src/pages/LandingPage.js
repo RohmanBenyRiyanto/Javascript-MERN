e18464ae4d53832e23344727711a58e531acbb71
RohmanBenyRiyanto <rendyasw@gmail.com>
2023-02-15 07:32:40 +0700

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}

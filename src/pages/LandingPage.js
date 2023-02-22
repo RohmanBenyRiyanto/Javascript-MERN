75b90377fdffc112bd0ca76c0d382a7332aedcf1
RohmanBenyRiyanto <rendyasw@gmail.com>
2023-02-22 21:04:23 +0700

    Slicing Most Pick

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Header from 'parts/Header';
 import landingPage from 'json/landingPage.json';
 import Hero from 'parts/Hero';
+import MostPick from 'parts/MostPick';
 
 export default class LandingPage extends Component {
     render() {
@@ -10,6 +11,7 @@ export default class LandingPage extends Component {
             <>
                 <Header {...this.props}></Header>
                 <Hero data={landingPage.hero} />
+                <MostPick data={landingPage.mostPicked} />
             </>
         )
     }

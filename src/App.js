57314a0d90571e06d905503ef20aca63507e5d56
RohmanBenyRiyanto <rendyasw@gmail.com>
2023-08-03 09:44:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import "assets/scss/style.scss";
+import './assets/scss/style.scss';
 import LandingPage from 'pages/LandingPage';
 
 

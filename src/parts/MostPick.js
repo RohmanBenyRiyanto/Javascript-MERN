1e96b661cf9e04897c89d5a09234b2c4eb3c88e5
Rohman Beny Riyanto <rendyasw@gmail.com>
2023-10-17 16:24:01 +0700

    update most pict view

--- src/parts/MostPick.js
+++ src/parts/MostPick.js
@@ -5,21 +5,16 @@ export default function MostPick(_props) {
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
       <div className="container-grid">
-        <div className="item column-4 row-2">
-          <div className="card">Card 1</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 2</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 3</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 4</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">Card 5</div>
-        </div>
+        {_props.data.map((item, index) => (
+          <div className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}>
+            <div className="card card-featured">
+              <div className="tag">
+                ${item.price}
+                {/* <span className="font-weight-light">per {item.unit}</span> */}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );

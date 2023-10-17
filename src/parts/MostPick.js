ca3f37711406a588ffa175b5ed1bfe7cae95311f
Rohman Beny Riyanto <rendyasw@gmail.com>
2023-10-17 15:11:48 +0700

    Update new react versions

--- src/parts/MostPick.js
+++ src/parts/MostPick.js
@@ -1,32 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import Button from 'components/Button'
-import Hero from 'parts/Hero'
-
-
-export default function MostPick(props) {
-    return (
-        <section className='container'>
-            <h4 className="mb-3">Most Picked</h4>
-            <div className="container-grid">
-                <div className="item column-4 row-2">
-                    <div className="card">Card 1</div>
-                </div>
-                <div className="item column-4 row-1">
-                    <div className="card">Card 2</div>
-                </div>
-                <div className="item column-4 row-1">
-                    <div className="card">Card 3</div>
-                </div>
-                <div className="item column-4 row-1">
-                    <div className="card">Card 4</div>
-                </div>
-                <div className="item column-4 row-1">
-                    <div className="card">Card 5</div>
-                </div>
-            </div>
-        </section>
-    )
+export default function MostPick(_props) {
+  return (
+    <section className="container">
+      <h4 className="mb-3">Most Picked</h4>
+      <div className="container-grid">
+        <div className="item column-4 row-2">
+          <div className="card">Card 1</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 2</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 3</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 4</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">Card 5</div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
